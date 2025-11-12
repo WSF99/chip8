2bf275877857c3ba293e8fac015f2aac68f5aac0
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-12 19:59:07 -0300

    add memory management header for CHIP-8 emulator

---
+++ src/memory.h
@@ -0,0 +1,11 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+#define MEM_SIZE 4096
+#include <stdint.h>
+#include <chip8.h>
+
+void memory_init(Memory *m);
+uint8_t memory_read(Memory *m, uint16_t addr);
+void memory_write(Memory *m, uint16_t addr, uint8_t val);
+int memory_load_rom(Memory *m, const char *path);
+#endif
