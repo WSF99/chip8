02bed4ee1cbffaa0fc6a3991b3bb302a21ef5f1e
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-12 23:35:34 -0300

    Add display_init function declaration to display.h

--- src/display.h
+++ src/display.h
@@ -2,6 +2,7 @@
 #define DISPLAY_H
 #include <chip8.h>
 
+void display_init(Display *disp);
 void display_clear(Display *disp);
 int display_draw_sprite(Display *d, Memory *m, uint16_t I, uint8_t x, uint8_t y, uint8_t n);
 
