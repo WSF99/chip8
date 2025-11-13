b32e3a0788a6dec937cfb87a08626ffa57509f0b
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-12 22:33:28 -0300

    add display.h header file with function declarations for display operations

---
+++ src/display.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include <chip8.h>
+
+void display_clear(Display *disp);
+int display_draw_sprite(Display *d, Memory *m, uint16_t I, uint8_t x, uint8_t y, uint8_t n);
+
+#endif // DISPLAY_H
