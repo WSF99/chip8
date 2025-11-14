50291595e6773b73ea81f8bb3dcf067247df9523
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-13 23:46:04 -0300

    Refactor input.h by updating key parameter types and removing unnecessary includes

--- src/input.h
+++ src/input.h
@@ -1,10 +1,15 @@
 #ifndef INPUT_H
 #define INPUT_H
+#include <stdint.h>
+#define NUM_KEYS 16
 
-#include "chip8.h"
+typedef struct Input {
+    uint8_t keys[NUM_KEYS];
+    uint8_t waiting_for_key;
+    uint8_t exit_signal;
+} Input;
 
 void input_init(Input *in);
-void input_set_key(Input *in, int key, int pressed);
-int input_any_pressed(Input *in);
-void input_wait_for_key(Input *in);
+void input_set_key(Input *in, uint8_t key, uint8_t pressed);
+void input_wait_for_key(Input *in, uint8_t register_idx);
 #endif // INPUT_H
