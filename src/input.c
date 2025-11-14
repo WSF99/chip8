1af023bf141ffe2656263c70972f8ed370b48f0d
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-13 23:45:59 -0300

    Refactor input.c by improving key handling functions and updating parameter types

--- src/input.c
+++ src/input.c
@@ -1,26 +1,21 @@
 #include "input.h"
+#include <stdio.h>
 
 void input_init(Input *in) {
     for (int i = 0; i < 16; i++) {
         in->keys[i] = 0;
     }
+    in->waiting_for_key = -1;
+    in->exit_signal = 0;
 }
 
-void input_set_key(Input *in, int key, int pressed) {
-    if (key >= 0 && key < 16) {
+
+void input_set_key(Input *in, uint8_t key, uint8_t pressed) {
+    if (key < 16) {
         in->keys[key] = pressed;
     }
 }
 
-int input_any_pressed(Input *in) {
-    for (int i = 0; i < 16; i++) {
-        if (in->keys[i]) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void input_wait_for_key(Input *in) {
-    in->waiting_for_key = -2;
+void input_wait_for_key(Input *in, uint8_t register_idx) {
+    in->waiting_for_key = register_idx;
 }
