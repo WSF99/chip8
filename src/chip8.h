5b2d6cf0d009d970e541ccc3d6ddc5fee1030dba
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-13 23:45:35 -0300

    Refactor chip8.h by removing unused definitions and consolidating struct declarations

--- src/chip8.h
+++ src/chip8.h
@@ -1,40 +1,16 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
-#include <stdint.h>
-
-#define START_ADDR 0x200
-#define FONTSET_ADDR 0x50
 #define NUM_REGS 16
 #define STACK_SIZE 16
-#define DISPLAY_WIDTH 64
-#define DISPLAY_HEIGHT 32
-#define MEM_SIZE 4096
-#define TIMER_HZ 60
 
+#include <stdint.h>
 #include "memory.h"
 #include "display.h"
 #include "input.h"
+#include "timers.h"
 
-typedef struct {
-    uint8_t memory[MEM_SIZE];
-} Memory;
-
-typedef struct {
-    uint8_t pixels[DISPLAY_WIDTH * DISPLAY_HEIGHT];
-} Display;
-
-typedef struct {
-    uint8_t keys[16];
-    int waiting_for_key;
-} Input;
-
-typedef struct {
-    uint8_t delay_timer;
-    uint8_t sound_timer;
-} Timers;
-
-typedef struct {
+typedef struct Chip8 {
     Memory mem;
     Display disp;
     Input input;
@@ -46,9 +22,8 @@ typedef struct {
     uint16_t stack[STACK_SIZE];
 } Chip8;
 
-extern const uint8_t chip8_fontset[80];
-
-void chip8_init(Chip8 *chip8);
-void chip8_reset(Chip8 *chip8);
+void chip8_init(Chip8 *c8);
+void chip8_reset(Chip8 *c8);
+void chip8_cycle(Chip8 *c8);
 
 #endif // CHIP8_H
