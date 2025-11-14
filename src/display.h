97af033c21d8c2478f04345db1d853844e6d9c6d
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-13 23:45:48 -0300

    Refactor display.h by removing unnecessary include and improving formatting

--- src/display.h
+++ src/display.h
@@ -1,9 +1,18 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
-#include <chip8.h>
+
+#define DISPLAY_WIDTH 64
+#define DISPLAY_HEIGHT 32
+
+#include <stdint.h>
+#include "memory.h"
+
+typedef struct Display {
+    uint8_t pixels[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+} Display;
 
 void display_init(Display *disp);
 void display_clear(Display *disp);
-int display_draw_sprite(Display *d, Memory *m, uint16_t I, uint8_t x, uint8_t y, uint8_t n);
+int display_draw_sprite(Display *disp, Memory *m, uint16_t I, uint8_t x, uint8_t y, uint8_t n);
 
 #endif // DISPLAY_H
