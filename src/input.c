5ea3711de9a974db6bb75c9bf038f5cceafdf7da
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-16 12:29:14 -0300

    Refactor input initialization for improved clarity and efficiency

--- src/input.c
+++ src/input.c
@@ -1,17 +1,15 @@
 #include "input.h"
+#include <string.h>
 
 void input_init(Input *in) {
-  for (int i = 0; i < 16; i++) {
-    in->keys[i] = 0;
-  }
+  memset(in->keys, 0, sizeof in->keys);
   in->waiting_for_key = -1;
   in->exit_signal = 0;
 }
 
 void input_set_key(Input *in, uint8_t key, uint8_t pressed) {
-  if (key < 16) {
+  if (key < 16)
     in->keys[key] = pressed;
-  }
 }
 
 void input_wait_for_key(Input *in, uint8_t register_idx) {
