336fa926df1e1ca1477ec5e92baa2d9d32ca4fe6
WSF99 <94298588+WSF99@users.noreply.github.com>
2025-11-16 12:29:39 -0300

    Refactor memory.h for improved clarity and consistency in memory definitions

--- src/memory.h
+++ src/memory.h
@@ -1,18 +1,16 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#define START_ADDR 0x200
-#define MEM_SIZE 4096
-#define FONTSET_ADDR 0x50
-
 #include <stdint.h>
 
-typedef struct Memory {
+enum { START_ADDR = 0x200, MEM_SIZE = 4096, FONTSET_ADDR = 0x50 };
+
+typedef struct {
   uint8_t memory[MEM_SIZE];
 } Memory;
 
 void memory_init(Memory *m);
-uint8_t memory_read(Memory *m, uint16_t addr);
+uint8_t memory_read(const Memory *m, uint16_t addr);
 void memory_write(Memory *m, uint16_t addr, uint8_t val);
 int memory_load_rom(Memory *m, const char *path);
 
